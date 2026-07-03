abad089a879fa0123de53db577344cf938dc9b44
Kenshi Sabio <kenshisabio09@gmail.com>
2026-07-03 19:58:05 +0800

    nano testing

---
+++ arduino-nano/led_test/led_test.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode(2, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); delay(50);
+  digitalWrite(2, LOW);
+
+  delay(1000);
+}
