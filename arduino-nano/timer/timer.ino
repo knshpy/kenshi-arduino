7266278581050b94f2b34581f4dd103ca65f873a
Kenshi Sabio <kenshisabio09@gmail.com>
2026-07-05 16:18:13 +0800

    led and piezzo buzzer timer

---
+++ arduino-nano/timer/timer.ino
@@ -0,0 +1,123 @@
+void setup() {
+  pinMode(2, OUTPUT); // Led
+  pinMode(3, OUTPUT); // Buzzer
+
+}
+
+void loop() {
+  // 10
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 9
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 8
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 7
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 6
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 5
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 4
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 3
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 2
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // 1
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(500);
+
+  // FINAL
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(100);
+
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(100);
+
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(5000);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+
+  delay(10000); // Wait 10seconds and repeat
+  
+}
